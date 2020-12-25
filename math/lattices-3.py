7acb40f8715870665cfd78fda15885882cfa6438
mowemcfc <jcartermcfc@gmail.com>
2020-12-25 20:16:05 +0800

    adds lattices-3 sol

--- math/lattices-3.py
+++ math/lattices-3.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
-def main(vecs):
-    vecs_u = [vecs[0],(),(),()]
-    for i in range(2,len(vecs) + 1):
-        for j in range(1,i):
+import numpy as np
 
-    
+def proj(u, v):
+    return np.inner(u,v) / np.inner(u,u) * u
+
+def main(A):
+    B = np.array([A[0], [0.0,0.0,0.0,0.0], [0.0,0.0,0.0,0.0], [0.0,0.0,0.0,0.0]])
+    for i in range(1,len(A)):
+        B[i] = A[i]
+        for j in range(0,i):
+            B[i] -= proj(B[j], A[i]) 
+
+    return B
 
 if __name__ == "__main__":
-    vecs = [(4,1,3,-1), (2,1,-3,4), (1,0,-2,7), (6,2,9,-5)]
-    main(vecs)
+    A = np.array([[4.0,1.0,3.0,-1.0], [2.0,1.0,-3.0,4.0], [1.0,0.0,-2.0,7.0], [6.0,2.0,9.0,-5.0]])
+    print(main(A))
